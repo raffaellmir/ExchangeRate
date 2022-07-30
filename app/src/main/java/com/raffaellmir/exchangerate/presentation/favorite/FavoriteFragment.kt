e9bc87158f0b37023920a91479cfcc1b528683ab
Raffaell M <raffaellmir@gmail.com>
2022-07-30 16:57:07 +0500

    add recyclerview and view binding

--- app/src/main/java/com/raffaellmir/exchangerate/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/raffaellmir/exchangerate/presentation/favorite/FavoriteFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.raffaellmir.exchangerate.R
+import com.raffaellmir.exchangerate.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/raffaellmir/exchangerate/presentation/popular/PopularFragment.kt
+++ app/src/main/java/com/raffaellmir/exchangerate/presentation/popular/PopularFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.raffaellmir.exchangerate.R
+import com.raffaellmir.exchangerate.databinding.FragmentPopularBinding
 
 class PopularFragment : Fragment() {
+    private var _binding: FragmentPopularBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_popular, container, false)
+    ): View {
+        _binding = FragmentPopularBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
